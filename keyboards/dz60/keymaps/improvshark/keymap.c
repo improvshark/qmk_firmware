2c53b5776bd60dbaf7caaada39a17ff89b7c689f
improvshark <improvshark@gmail.com>
2018-06-23 20:35:05 -0600

    first revision

--- keyboards/dz60/keymaps/improvshark/keymap.c
+++ keyboards/dz60/keymaps/improvshark/keymap.c
@@ -2,36 +2,34 @@
 
 #define _______ KC_TRNS
 #define ___X___ KC_NO
-#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
-
-#define RGB_STA RGB_M_P //rgb static
-#define RGB_BRE RGB_M_B //rgb breathe
-#define RGB_RAI RGB_M_R //rgb rainbow
-#define RGB_SWI RGB_M_SW //rgb swirl
-#define RGB_SNA RGB_M_SN //rgb snake
-#define RGB_KNI RGB_M_K //rgb knight
-#define RGB_GRA RGB_M_G //rgb gradient
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+	/*		Base Layer
+	 */
 	LAYOUT(
-		KC_GESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , ___X___, KC_BSPC,
+		KC_GESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, KC_DEL,
 		KC_TAB  ,          KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
 		LT(MO(1), KC_CAPS),KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,          KC_ENT ,
-		KC_LSFT , ___X___, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,          ___X___,
-		KC_LCTL ,          KC_LGUI, KC_LALT, KC_SPC ,          KC_SPC ,          KC_SPC , KC_RALT, KC_RGUI, ___X___, MO(2)  ,          KC_RCTL),
-
+		KC_LSFT , ___X___, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,          TO(2)  ,
+    KC_LCTL ,          KC_LGUI, KC_LALT, MO(1)  ,          MO(1)           , KC_SPC , KC_RALT, KC_RGUI, ___X___, KC_MENU,          KC_RCTL),
+	/*
+		Function Layer
+	 */
 	LAYOUT(
-		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , ___X___, KC_DEL ,
-		_______,          _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______, _______, _______, _______,
-		_______,          _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______, _______,          _______,
-		_______, ___X___, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-		_______,          _______, _______, _______,          _______,          _______, _______, _______, _______, _______,          _______),
+		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10  , KC_F11 , KC_F12 , KC_PSCR, KC_INS ,
+		_______,          _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  , _______, _______, _______, _______,
+		_______,          _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______, _______,          _______,
+		_______, ___X___, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, _______,          _______,
+		_______,          _______, _______, _______,          TO(0)  ,          _______, _______, _______ , ___X___, _______,          RESET  ),
 
-LAYOUT(
-		RESET  , M(1)   , M(2)   , M(3)   , M(4)   , M(5)   , M(6)   , M(7)   , M(8)   , M(9)   , M(10)  , M(11)  , M(12)  , ___X___, KC_SLEP,
-		_______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,
-		_______,          RGB_STA, RGB_BRE, RGB_RAI, RGB_SWI, RGB_SNA, RGB_KNI, RGB_GRA, _______, KC_MPRV, KC_MPLY, KC_MNXT,          _______,
-		_______, ___X___, BL_DEC , BL_TOGG, BL_INC , BL_STEP, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE,          _______,
-		_______,          _______, _______, _______,          _______,          _______, _______, _______, _______, _______,          _______),
+	 /*
+		RGB Layer
+	 */
+	LAYOUT(
+		RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_K, RGB_M_G, RGB_M_SW, RGB_M_SN, _______, _______, _______, _______, _______, _______, RGB_M_X,
+		_______,          KC_ACL0, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U,  _______,  RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______,
+		_______,          KC_ACL1, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,  _______, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, _______,          _______,
+		_______, ___X___, KC_ACL2, _______, _______, _______, _______,  _______,  _______, _______, _______, _______, _______,          TO(0)  ,
+		_______,          _______, _______, _______,          TO(0)  ,            _______, _______, _______, ___X___, _______,          _______)
 };
