8bdd6f34a6ab570913979d026859ddc3b907b094
improvshark <improvshark@gmail.com>
2018-12-20 16:18:25 -0700

    few tweeks

--- keyboards/dz60/keymaps/improvshark/keymap.c
+++ keyboards/dz60/keymaps/improvshark/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB  ,          KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
 		LT(MO(1), KC_CAPS),KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,          KC_ENT ,
 		KC_LSFT , ___X___, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,          TD(0)  ,
-    KC_LCTL ,          KC_LGUI, KC_LALT, LT(MO(1), KC_SPC)  ,              KC_F12          , KC_SPC , KC_RALT, KC_RGUI, ___X___, KC_MENU,          KC_RCTL),
+		KC_LCTL ,          KC_LGUI, KC_LALT, LT(MO(1), KC_SPC)  ,              KC_F12          , KC_SPC , KC_RALT, KC_RGUI, ___X___, KC_MENU,   KC_RCTL),
 	/*
 		Function Layer
 	 */
@@ -29,16 +29,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
 		_______, ___X___, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          TO(0)  ,
-		_______,          _______, _______,  KC_SPC,           KC_SPC,           KC_SPC, _______, _______, ___X___, _______,          _______),
+		_______,          _______, _______,  KC_SPC,           MO(1) ,           KC_SPC, _______, _______, ___X___, _______,          _______),
 	 /*
 		RGB Layer
 	 */
 	LAYOUT(
 		RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_K, RGB_M_G, RGB_M_SW, RGB_M_SN, _______, _______, _______, _______, _______, _______, RGB_M_X,
-		_______,          KC_ACL0, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U,  _______,  RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______,
-		_______,          KC_ACL1, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,  _______, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, _______,          _______,
-		_______, ___X___, KC_ACL2, _______, _______, _______, _______,  _______,  _______, _______, _______, _______, _______,          TO(0)  ,
-		_______,          _______, _______, _______,          TO(0)  ,            _______, _______, _______, ___X___, _______,          _______)
+		_______,          _______, _______, _______, _______, _______,  _______,  RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______,
+		_______,          _______, _______, _______, _______, _______,  _______, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, _______,          _______,
+		_______, ___X___, _______, _______, _______, _______, _______,  _______,  _______, _______, _______, _______, _______,          TO(0)  ,
+		_______,          _______, _______, TO(0)  ,          TO(0)  ,            _______, _______, _______, ___X___, _______,          _______)
 };
 
 void changeLayer (qk_tap_dance_state_t *state, void *user_data) {
